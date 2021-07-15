7448842d24150823a3178f7dd5bf61973d7b250c
ekarazas <karazas7@gmail.com>
2021-07-15 03:27:02 +0300

    minor fixes

--- src/routes/v1/content.js
+++ src/routes/v1/content.js
@@ -10,16 +10,19 @@ router.get("/teams", isLoggedIn, async (req, res) => {
     const con = await mysql.createConnection(mysqlConfig);
 
     const [data] = await con.execute("SELECT * FROM teams");
+    
     con.end();
 
     if (data.length === 0) {
-      return res.send({ message: "No teams" });
+      return res.send({ message: "There are no teams" });
     }
 
     return res.send(data);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send({ error: "Database error" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ error: "An unexpected error occured. Please try again" });
   }
 });
 
